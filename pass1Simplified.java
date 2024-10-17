af546d50551d8fb6f7284afd1cd3e8e50be6e401
$@#il <sahilsadekar249775@gmail.com>
2024-10-18 02:54:15 +0530

    pass 1 easiest approach

---
+++ pass1Simplified.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class pass1Simplified {
+    // superGlobals array whihc container symbol table ingotmation and inetmedaite
+    // code
+    Map<String, Integer> symbolTable = new HashMap<>();
+    List<String> intermediateCode = new ArrayList<>();
+    int locationCounter = 0;
+
+    // pass one stage method
+    void passOne(String[] sourceCode) {
+        // get line by line code
+        for (String line : sourceCode) {
+            String[] labeledInstructions = line.split(":");
+            String label = labeledInstructions[0].trim();
+            symbolTable.put(label, locationCounter);
+
+            // process instruction part if avalieble
+            if (labeledInstructions.length > 1) {
+               //means instructon also present
+               String instructionPart=labeledInstructions[1].trim();
+               //add
+               //increment location counter
+               //done
+            }
+        }
+    }
+
+    public void printSymbolTable() {
+        System.out.println(symbolTable);
+    }
+
+    public void printIntermediateCode() {
+        System.out.println(intermediateCode);
+    }
+
+    public static void main(String args[]) {
+        String[] sourceCode = {
+                "START",
+                "LOOP: LOAD A",
+                "ADD B",
+                "STORE C",
+                "END"
+        };
+
+        pass1Simplified ps1 = new pass1Simplified();
+        ps1.passOne(sourceCode); // sent source code to pass 1
+        ps1.printIntermediateCode();
+        ps1.printSymbolTable();
+
+    }
+}
